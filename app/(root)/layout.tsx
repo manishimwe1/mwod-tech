8f9b58090e8483d657f411af1c42f8fa4a661335
manishimwe1 <emmymanishimwem@gmail.com>
2026-01-21 15:39:47 +0200

    feat(metadata): enhance SEO and social media metadata
    Update metadata configuration to improve SEO and social media sharing. Added dynamic title template, expanded keywords, and included openGraph/twitter metadata for better link previews. Also added author, creator, and publisher information.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,9 +5,61 @@ import Footer from "@/components/Footer";
 
 
 export const metadata: Metadata = {
-  title: "mwod technology - Electronics Repair & Shop",
+  title: {
+    default:
+      "Buy Laptops in Rwanda | New & Used Computers in Kigali – mwod Technology",
+    template: "%s | mwod Technology",
+  },
   description:
-    "Your one-stop destination for electronics repair services and new device purchases. Expert repairs, great deals, and quality electronics.",
+    "Buy affordable new and used laptops in Rwanda. mwod Technology Kigali offers HP, Dell, Lenovo computers, accessories & fast delivery across Rwanda.",
+  keywords: [
+    "electronics repair",
+    "buy electronics",
+    "sell electronics",
+    "tech services",
+    "gadgets",
+    "smartphones",
+    "laptops",
+    "accessories",
+    "electronics store Rwanda",
+    "buy laptops Rwanda",
+    "sell electronics Rwanda",
+    "electronics repair Rwanda",
+    "phone repair Kigali",
+    "laptops and accessories",
+    "smartphones Rwanda",
+    "tech shop Kigali",
+  ],
+  authors: [{ name: "emino" }],
+  creator: "mwod Technology",
+  publisher: "mwod Technology",
+  openGraph: {
+    title: "mwod Technology - Sell and Buy some of electronics",
+    description:
+      "Your one-stop destination for electronics repair services and new device purchases. Expert repairs, great deals, and quality electronics.",
+    url: "https://mwod-tech.vercel.app/", // Replace with your actual URL
+    siteName: "mwod Technology",
+    images: [
+      {
+        url: "https://mwod-tech.vercel.app/logo1.png", // Replace with your actual OG image
+        width: 1200,
+        height: 630,
+        alt: "mwod Technology - Electronics Repair and Sales",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "mwod Technology - Sell and Buy some of electronics",
+    description:
+      "Your one-stop destination for electronics repair services and new device purchases. Expert repairs, great deals, and quality electronics.",
+    // images: ["https://mwod-tech.vercel.app//twitter-image.jpg"], // Replace with your actual Twitter image
+    creator: "@eminodev", // Replace with your Twitter handle
+    images: ["https://mwod-tech.vercel.app/logo1.png"],
+  },
+  icons: ["https://mwod-tech.vercel.app/logo1.png"],
 };
 
 export default function RootLayout({
